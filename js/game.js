30762b91c10a71c22d10c0aefc551783fb4c13a0
vzazueta <A00226702@itesm.mx>
2021-07-14 17:58:05 -0700

    [GAME] Began score.js module, where we will later add database functions; began structuring game.js data into 'Pair' objects, which contain important information on the cards being flipped. Most movements are done in checkPairs() function

--- js/game.js
+++ js/game.js
@@ -1,12 +1,5 @@
 let cards_folder_path = "./cards/all/";
 let PNG = ".png";
-let back_cover_path = "./cards/back_covers/Emerald" + PNG;
-let start_button = document.getElementById("start-button");
-let img_array = document.getElementsByClassName("card");
-let card_pairs_indeces;
-let keys_and_images;
-let used_images;
-let score_text = document.getElementById("score");
 
 let card_paths_array = [
     cards_folder_path + "C2" + PNG,
@@ -66,14 +59,30 @@ let card_paths_array = [
     cards_folder_path + "SQ" + PNG
 ];
 
+import { setScoreZero, changeScore } from "./score.js";
+
+let back_cover_path = "./cards/back_covers/Emerald" + PNG;
+let start_button = document.getElementById("start-button");
+let img_array = document.getElementsByClassName("card");
+let previous_card;
+let cards_flipped;
+let card_pairs_indeces;
+let keys_and_images;
+let used_images;
+let existing_pairs;
+
 start_button.addEventListener("click", start);
 
 function start(){
     this.innerHTML = "Restart Game";
-    score = 0;
+    setScoreZero();
+    cards_flipped = 0;
+    card_pairs_indeces = {};
+    keys_and_images = {};
+    used_images = [];
+    existing_pairs = [];
 
-    for(let i=0; i<img_array.length; i++){
-        let image = img_array[i];
+    for(let image of img_array){
         image.src = back_cover_path;
         image.addEventListener("click", flipCard);
     }
@@ -82,9 +91,6 @@ function start(){
 }
 
 function shuffle(){
-    card_pairs_indeces = {};
-    keys_and_images = {};
-    used_images = [];
     let key = 0;
     let length = 0;
 
@@ -102,6 +108,8 @@ function shuffle(){
         card_pairs_indeces[key++] = value;
         length++;
     }
+
+    createPairs();
 }
 
 function findInMap(index){
@@ -127,9 +135,9 @@ function setRandomImage(key){
 
 function flipCard(){ 
     let back_cover = back_cover_path.replace('.', '');
+    let id = parseInt(this.id);
 
     if(this.src.includes(back_cover)) {
-        let id = parseInt(this.id);
         let entries = Object.entries(card_pairs_indeces);
         
         for(let [k, v] of entries) {
@@ -140,22 +148,75 @@ function flipCard(){
         }
 
         this.src = keys_and_images[id];
-    } else {
-        this.src = back_cover_path;
+        changeCardState(id, true);
+        this.removeEventListener("click", flipCard);
+        cards_flipped++;
     }
+
+    if(cards_flipped % 2 === 0) checkPairs(id);
+    else previous_card = this;
 }
 
-function checkCards(){
+function checkPairs(id){
+    for(let pair of existing_pairs){
+        if(!pair.found) {
+            let c1 = pair.card_one;
+            let c2 = pair.card_two;
 
-}
+            if(c1[0] === id) { 
+                if(c1[2]) {
+                    if(c2[2]) {
+                        pair.found = true;
+                        changeScore(true);
+                    } else {
+                        c1[1].src = back_cover_path;
+                        c1[1].addEventListener("click", flipCard);
+                        c1[2] = false;
+                        changeScore(false);
+                    } 
+                }
 
-function changeScore(add){
-    let score = parseInt(score_text.innerHTML);
-    
-    if(add) score_text.innerHTML = ++score;
-    else score_text.innerHTML = --score;
+                break;
+            } else if(c2[0] === id) {
+                if(c2[2]) { 
+                    if(c1[2] === true) {
+                        pair.found = true;
+                        changeScore(true);
+                    } else {
+                        c2[1].src = back_cover_path;
+                        c2[1].addEventListener("click", flipCard);
+                        changeScore(false);
+                    }
+                }
+
+                break;
+            }
+        }
+    }
 }
 
 function randnum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function createPairs(){
+    let k_and_v = Object.entries(card_pairs_indeces);
+
+    for(let [k, v] of k_and_v) {
+        let pair = {
+            card_one : [k, img_array[k], false],
+            card_two : [v, img_array[v], false],
+            src : keys_and_images[k],
+            found : false
+        }
+
+        existing_pairs.push(pair);
+    }
+}
+
+function changeCardState(id, new_state){
+    for(let pair of existing_pairs){
+        if(pair.card_one[0] === id) pair.card_one[2] = new_state;
+        else if(pair.card_two[0] === id) pair.card_two[2] = new_state;
+    }
 }
---
+++ js/score.js
@@ -0,0 +1,12 @@
+let score_text = document.getElementById("score");
+
+export function setScoreZero(){
+    score_text.innerHTML = "0";
+}
+
+export function changeScore(add){
+    let score = parseInt(score_text.innerHTML);
+    
+    if(add) score_text.innerHTML = ++score;
+    else score_text.innerHTML = --score;
+}
