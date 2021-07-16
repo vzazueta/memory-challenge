8eaf7aa6b6ba82eb46f7883dd675c044490e1618
vzazueta <A00226702@itesm.mx>
2021-07-16 11:34:42 -0700

    [SCORE] We use localStorage to keep the information about each game

--- js/score.js
+++ js/score.js
@@ -1,6 +1,16 @@
+import { print } from './misc.js';
+
 let score_text = document.getElementById("score");
 let username;
 let final_score;
+//let xhr = new XMLHttpRequest();
+//let site = "http://something.com";
+
+xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4){
+        print("Score uploaded succesfully!");
+    }
+};
 
 export function setScoreZero(){
     score_text.innerText = "0";
@@ -16,8 +26,8 @@ export function changeScore(add){
 export function finishGame(){
     final_score = parseInt(score_text.innerText);
     username = prompt("Finished!\n\n"+
-                        "Please, enter your username:", 
-                        "Victor Zazueta");
+                    "Please, enter your username:", 
+                    "Victor Zazueta");
 
     let play_again = confirm("Your score: "+final_score+"\n\n"+
                             "Play Again?");
@@ -27,6 +37,14 @@ export function finishGame(){
     return play_again;
 }
 
+// We will use local storage for now
 function sendToDatabase(){
-    
+    //xhr.open('POST', site);
+
+    let user_and_score = {
+        user : username,
+        score : final_score
+    }
+
+    localStorage.setItem("game_info", user_and_score);
 }
