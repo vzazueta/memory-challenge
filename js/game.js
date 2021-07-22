06cd2464f1c83ec8c120dfa65afa4184724971dc
vzazueta <A00226702@itesm.mx>
2021-07-22 15:26:12 -0700

    [GAME] I was going to add some complex function to avoid stepping on flipped cards, but then it became too hard so if the card is inn flipped_cards array it just warns the player about it

--- js/game.js
+++ js/game.js
@@ -1,70 +1,7 @@
-let cards_folder_path = "./cards/all/";
-let PNG = ".png";
-
-let card_paths = [
-    cards_folder_path + "C2" + PNG,
-    cards_folder_path + "C3" + PNG,
-    cards_folder_path + "C4" + PNG,
-    cards_folder_path + "C5" + PNG,
-    cards_folder_path + "C6" + PNG,
-    cards_folder_path + "C7" + PNG,
-    cards_folder_path + "C8" + PNG,
-    cards_folder_path + "C9" + PNG,
-    cards_folder_path + "C10" + PNG,
-    cards_folder_path + "CA" + PNG,
-    cards_folder_path + "CJ" + PNG,
-    cards_folder_path + "CK" + PNG,
-    cards_folder_path + "CQ" + PNG,
-
-    cards_folder_path + "D2" + PNG,
-    cards_folder_path + "D3" + PNG,
-    cards_folder_path + "D4" + PNG,
-    cards_folder_path + "D5" + PNG,
-    cards_folder_path + "D6" + PNG,
-    cards_folder_path + "D7" + PNG,
-    cards_folder_path + "D8" + PNG,
-    cards_folder_path + "D9" + PNG,
-    cards_folder_path + "D10" + PNG,
-    cards_folder_path + "DA" + PNG,
-    cards_folder_path + "DJ" + PNG,
-    cards_folder_path + "DK" + PNG,
-    cards_folder_path + "DQ" + PNG,
-
-    cards_folder_path + "H2" + PNG,
-    cards_folder_path + "H3" + PNG,
-    cards_folder_path + "H4" + PNG,
-    cards_folder_path + "H5" + PNG,
-    cards_folder_path + "H6" + PNG,
-    cards_folder_path + "H7" + PNG,
-    cards_folder_path + "H8" + PNG,
-    cards_folder_path + "H9" + PNG,
-    cards_folder_path + "H10" + PNG,
-    cards_folder_path + "HA" + PNG,
-    cards_folder_path + "HJ" + PNG,
-    cards_folder_path + "HK" + PNG,
-    cards_folder_path + "HQ" + PNG,
-
-    cards_folder_path + "S2" + PNG,
-    cards_folder_path + "S3" + PNG,
-    cards_folder_path + "S4" + PNG,
-    cards_folder_path + "S5" + PNG,
-    cards_folder_path + "S6" + PNG,
-    cards_folder_path + "S7" + PNG,
-    cards_folder_path + "S8" + PNG,
-    cards_folder_path + "S9" + PNG,
-    cards_folder_path + "S10" + PNG,
-    cards_folder_path + "SA" + PNG,
-    cards_folder_path + "SJ" + PNG,
-    cards_folder_path + "SK" + PNG,
-    cards_folder_path + "SQ" + PNG
-];
-
+import { card_paths, back_cover_path, start_button, img_array } from "./variables.js";
 import { setScoreZero, changeScore, finishGame } from "./score.js";
 import { print, randnum } from "./misc.js";
 
-let back_cover_path = "./cards/back_covers/Emerald" + PNG;
-let start_button = document.getElementById("start-button");
-let img_array = document.getElementsByClassName("card");
 let previous_card, actual_card;
 let wrong_pair;
 let cards_flipped;
@@ -75,159 +12,7 @@ let selected_card;
 
 start_button.addEventListener("click", start);
 
-window.addEventListener("keydown", function(event) {
-    if (event.defaultPrevented)
-      return; // Do nothing if event already handled
-
-    setCardBrightness(100);
-
-    let direction = event.code;
-    let id = parseInt(selected_card.id);
-    let line = parseInt((id / 4) + 1);
-    let prev_id = -1;
-    let actual_id = -1;
-
-    if(previous_card) prev_id = parseInt(previous_card.id);
-    if(actual_card) actual_id = parseInt(actual_card.id);
-
-    print(prev_id+", "+actual_id);
-
-    switch(direction) {
-        case "ArrowDown":
-            id += 4;
-            
-            if(id > 15) id -= 16;
-
-            while(id === prev_id ||
-                id === actual_id){
-                    id += 4;
-                    if(id > 15) id -= 16;
-            }
-
-            break;
-        case "ArrowUp":
-            id -= 4;
-            if(id < 0) id += 16;
-
-            while(id === prev_id ||
-                id === actual_id){
-                    id -= 4;
-                    if(id < 0) id += 16;
-            }
-
-            break;
-        case "ArrowLeft":
-            switch(line) {
-                case 1:
-                    id--;
-                    if(id < 0) id += 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id--;
-                            if(id < 0) id += 4;
-                    }
-
-                    break;
-                case 2:
-                    id--;
-                    if(id < 4) id += 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id--;
-                            if(id < 4) id += 4;
-                    }
-
-                    break;
-                case 3:
-                    id--;
-                    if(id < 8) id += 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id--;
-                            if(id < 8) id += 4;
-                    }
-
-                    break;
-                case 4:
-                    id--;
-                    if(id < 12) id += 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id--;
-                            if(id < 12) id += 4;
-                    }
-
-                    break;
-            }
-
-            break;
-        case "ArrowRight":
-            switch(line) {
-                case 1:
-                    id++;
-                    if(id > 3) id -= 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id++;
-                            if(id > 3) id -= 4;
-                    }
-
-                    break;
-                case 2:
-                    id++;
-                    if(id > 7) id -= 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id++;
-                            if(id > 7) id -= 4;
-                    }
-
-                    break;
-                case 3:
-                    id++;
-                    if(id > 11) id -= 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id++;
-                            if(id > 11) id -= 4;
-                    }
-
-                    break;
-                case 4:
-                    id++;
-                    if(id > 15) id -= 4;
-
-                    while(id === prev_id ||
-                        id === actual_id){
-                            id++;
-                            if(id > 15) id -= 4;
-                    }
-
-                    break;
-            }
-
-            break;
-        case "Enter":
-            flipCard(selected_card);
-        break;
-    }
-
-    //while(isFound(id)) {
-    //    id = findBestCard(id, direction);
-    //}
-
-    selected_card = img_array[id];
-    setCardBrightness(150);
-
-    event.preventDefault();
-});
+window.addEventListener("keydown", move);
 
 /* 
     Whenever the 'Start New Game' is clicked or
@@ -426,30 +211,99 @@ function createPair(card_one_id, card_two_id){
     });
 }
 
+function move(event) {
+    if (event.defaultPrevented)
+      return; // Do nothing if event already handled
+
+    setCardBrightness(100);
+
+    let direction = event.code;
+    let id = parseInt(selected_card.id);
+    let line = parseInt((id / 4) + 1);
+
+    switch(direction) {
+        case "ArrowDown":
+            id += 4;
+            if(id > 15) id -= 16;
+
+            break;
+        case "ArrowUp":
+            id -= 4;
+            if(id < 0) id += 16;
+
+            break;
+        case "ArrowLeft":
+            switch(line) {
+                case 1:
+                    id--;
+                    if(id < 0) id += 4;
+
+                    break;
+                case 2:
+                    id--;
+                    if(id < 4) id += 4;
+
+                    break;
+                case 3:
+                    id--;
+                    if(id < 8) id += 4;
+
+                    break;
+                case 4:
+                    id--;
+                    if(id < 12) id += 4;
+
+                    break;
+            }
+
+            break;
+        case "ArrowRight":
+            switch(line) {
+                case 1:
+                    id++;
+                    if(id > 3) id -= 4;
+
+                    break;
+                case 2:
+                    id++;
+                    if(id > 7) id -= 4;
+
+                    break;
+                case 3:
+                    id++;
+                    if(id > 11) id -= 4;
+
+                    break;
+                case 4:
+                    id++;
+                    if(id > 15) id -= 4;
+
+                    break;
+            }
+
+            break;
+        case "Enter":
+            if(!isFound(id)) flipCard(selected_card);
+            else alert("Already flipped!");
+        break;
+    }
+
+    //while(isFound(id)) {
+    //    id = findBestCard(id, direction);
+    //}
+
+    selected_card = img_array[id];
+
+    if(!isFound(id)) setCardBrightness(130);
+    else setCardBrightness(70);
+    
+    event.preventDefault();
+}
+
 function setCardBrightness(percent){
     selected_card.style.filter = "brightness(" + percent + "%)";
 }
 
 function isFound(id){
     return found_pairs.includes(id);
-}
-
-function findBestCard(id, direction){
-    switch(direction) {
-        case "ArrowDown":
-            
-            break;
-        
-        case "ArrowUp":
-
-            break;
-
-        case "ArrowRight":
-
-            break;
-
-        case "ArrowLeft":
-
-            break;
-    }
 }
